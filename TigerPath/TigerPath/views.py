9ffe9e7c954a231877842fb7a120b386a43a3060
William Hinthorn <hinthorn@princeton.edu>
2016-03-31 21:43:49 -0400

    Added a commented out version of four_year
    Uses the ManyToManyField() implementation of the model

--- TigerPath/TigerPath/views.py
+++ TigerPath/TigerPath/views.py
@@ -18,3 +18,17 @@ def four_year(request, s_id):
 				c_names.append(c) 
 	context = {'student_courses_list': student_courses_list,'student_id': s_id, 'c_names': c_names, 'student': student}
 	return render(request, 'TigerPath/four_year.html', context)
+
+
+##Will's new four_year file based on ManyToManyField representation
+# def four_year(request, s_id):
+"""Filters data for presentation. NOT CHECKED YET (I still can't use MySQL) """
+	# c_names=[]
+	# student_courses_list = StudentCourse.objects.filter(student_id=s_id)
+	# individual = Student.objects.filter(student_id=s_id)
+	# student = individual[0].first_name + " " + individual[0].last_name
+	# courses_list = Course.objects.all()
+	# for s in individual.student_courses:
+	# 	c_names.append(c) 
+	# context = {'student_courses_list': student_courses_list,'student_id': s_id, 'c_names': c_names, 'student': student}
+	# return render(request, 'TigerPath/four_year.html', context)
