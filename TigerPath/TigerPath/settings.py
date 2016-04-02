ecfb754c7059fb3eb34b0d345241f19bf366aa4d
Viveque Ramji <viveque.ramji@gmail.com>
2016-04-02 16:05:17 -0400

    Update settings.py

--- TigerPath/TigerPath/settings.py
+++ TigerPath/TigerPath/settings.py
@@ -19,6 +19,20 @@ import dj_database_url
 #Entered for heroku
 #db_from_env = dj_database_url.config(conn_max_age=500)
 #DATABASES['default'].update(db_from_env)
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+#STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+#STATICFILES_DIRS = (
+#    os.path.join(PROJECT_ROOT, 'static'),
+#)
+
+###########HEROKU END
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
