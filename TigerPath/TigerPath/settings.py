9cf368052db8323b249945263317985dec7b8a12
Viveque Ramji <viveque.ramji@gmail.com>
2016-04-03 16:33:35 -0400

    Update settings.py

--- TigerPath/TigerPath/settings.py
+++ TigerPath/TigerPath/settings.py
@@ -111,7 +111,10 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+#Entered for heroku
 
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
