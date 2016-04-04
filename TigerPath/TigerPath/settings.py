9485e342a8fa0061fdc63a14ee842f3e453ee544
Viveque Ramji <viveque.ramji@gmail.com>
2016-04-03 23:10:26 -0400

    Update settings.py

--- TigerPath/TigerPath/settings.py
+++ TigerPath/TigerPath/settings.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cas', ### I dont think this is right? seems to crash the app
+    #'cas', ### I dont think this is right? seems to crash the app
 ]
 
 MIDDLEWARE_CLASSES = [
