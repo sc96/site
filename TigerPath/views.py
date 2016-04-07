3dfbc1b4e90818e7f055c9438a6a2c082ab72c60
Viveque Ramji <viveque.ramji@gmail.com>
2016-04-07 14:17:30 -0400

    Update views.py

--- TigerPath/views.py
+++ TigerPath/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Student, Course, StudentCourse, COS_BSE
+from .models import Student, Course,  COS_BSE #StudentCourse,
 from django.contrib.auth.decorators import login_required
 
 @login_required # Cas authentication for this url.
