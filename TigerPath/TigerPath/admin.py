bcee82231f2f1740f1f620635ce3f0ae84d21566
Viveque Ramji <viveque.ramji@gmail.com>
2016-04-07 11:49:17 -0400

    Delete admin.py

--- TigerPath/TigerPath/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Student, Course, StudentCourse, COS_BSE
-
-admin.site.register(Student)
-#admin.site.register(Course)
-#admin.site.register(StudentCourse)
-#admin.site.register(COS_BSE)
