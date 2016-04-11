d0cddca68cd32952085e58131d266569ac1ca27b
nwertz <wertznatalie@gmail.com>
2016-04-11 14:27:04 -0400

    cas authentication

--- TigerPath/views.py
+++ TigerPath/views.py
@@ -151,7 +151,7 @@ def degree_progress(request):
 		'iw_on': iw_on, 'iw_off': iw_off, 'core_on': core_on, 'core_off': core_off, 'other_theory': other_theory,
 		'student_sa': student_sa, 'student_la': student_la, 'student_ha': student_ha, 'student_ec': student_ec,
 		'student_em': student_em, 'student_foreign': student_foreign, 'student_wri': student_wri, 'outside_courses': student_outside}
-		return render(request, 'TigerPath/degree_progress_cos_bse.html', context)
+		return render(request, 'pages/degree_progress_cos_bse.html', context)
  
 @login_required # Cas authentication for this url.
 def four_year(request):
@@ -175,7 +175,7 @@ def four_year(request):
 	context = {'user': current_user.username,'fresh_fall': fresh_fall, 'fresh_spring': fresh_spring, 
 	'soph_fall': soph_fall, 'soph_spring': soph_spring, 'junior_fall': junior_fall, 'junior_spring': junior_spring,
 	'senior_fall': senior_fall, 'senior_spring': senior_spring}
-	return render(request, 'TigerPath/four_year.html', context)
+	return render(request, 'pages/four_year.html', context)
 
 @login_required # Cas authentication for this url.
 # if you got a course at Princeton to count as a COS departmental
