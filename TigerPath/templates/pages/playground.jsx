fb280ede8f4bba0804bced8e28827ad5830c72f9
Viveque Ramji <vivequeramji@nat-oitwireless-inside-vapornet100-e-978.Princeton.EDU>
2016-04-12 17:30:54 -0400

    vr

---
+++ TigerPath/templates/pages/playground.jsx
@@ -0,0 +1,14 @@
+var Message = React.createClass({
+  render: function(){
+    return (
+
+      <div>
+        <p className='text'>easydevtuts</p>
+        <p className='text'>tutorials</p>
+      </div>
+
+    );
+  }
+});
+
+React.render(<Message />, document.getElementById('demo'));
