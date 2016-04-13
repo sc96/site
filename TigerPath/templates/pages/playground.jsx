6fbbf7fd7637d66d096860ef21b7ac154cd2dc9a
vivequeramji <vivequeramji@gmail.com>
2016-04-13 14:04:21 -0400

    vr

--- TigerPath/templates/pages/playground.jsx
+++ TigerPath/templates/pages/playground.jsx
@@ -1,14 +1,11 @@
 var Message = React.createClass({
   render: function(){
     return (
-
       <div>
         <p className='text'>easydevtuts</p>
         <p className='text'>tutorials</p>
       </div>
-
     );
   }
 });
-
 React.render(<Message />, document.getElementById('demo'));
