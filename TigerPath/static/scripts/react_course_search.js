232ee4eb1fa45b9643e69eed0e853cbea70621f7
William Hinthorn <hinthorn@princeton.edu>
2016-04-15 18:13:38 -0400

    Test button

--- TigerPath/static/scripts/react_course_search.js
+++ TigerPath/static/scripts/react_course_search.js
@@ -1,4 +1,27 @@
  var login = "{{ user }}"
+
+ var LikeButton = React.createClass({
+  getInitialState: function() {
+    return {liked: false};
+  },
+  handleClick: function(event) {
+    this.setState({liked: !this.state.liked});
+  },
+  render: function() {
+    var text = this.state.liked ? 'like' : 'haven\'t liked';
+    return (
+      <p onClick={this.handleClick}>
+        You {text} this. Click to toggle.
+      </p>
+    );
+  }
+});
+
+ReactDOM.render(
+  <LikeButton />,
+  document.getElementById('example')
+);
+
   var CourseSearch = React.createClass({
      getInitialState: function() {
       return {
