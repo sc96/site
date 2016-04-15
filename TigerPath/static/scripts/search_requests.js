c2b9e1727e315a0df763e5a02ed9bfbca1904dd0
William Hinthorn <hinthorn@princeton.edu>
2016-04-15 18:58:13 -0400

    rv

--- TigerPath/static/scripts/search_requests.js
+++ TigerPath/static/scripts/search_requests.js
@@ -3,6 +3,7 @@ console.log("Printing the tango from Django");
 $(document).ready( function() {
     console.log("document is ready");
     $("#search-btn").click( function(event) {
+        event.preventDefault();
         console.log("Click thing worked");
         alert("You clicked the button using JQuery!");
     });
