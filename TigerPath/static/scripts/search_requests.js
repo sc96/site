dbcab429f70137d8e44001f997fdd7fcae10b678
Viveque Ramji <viveque.ramji@gmail.com>
2016-04-15 20:13:13 -0400

    Delete search_requests.js

--- TigerPath/static/scripts/search_requests.js
+++
@@ -1,8 +0,0 @@
-$(document).ready( function() {
-    console.log("document is ready");
-    $("search-btn").click( function(event) {
-        event.preventDefault();
-        console.log("Click thing worked");
-        alert("You clicked the button using JQuery!");
-    });
-});
