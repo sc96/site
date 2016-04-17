7f594317aa7612456b7c9d2106985e0c2b4a537b
sc96 <chengsam96@yahoo.com>
2016-04-17 15:21:28 -0400

    cas log out

--- TigerPath/cas/__init__.py
+++ TigerPath/cas/__init__.py
@@ -9,7 +9,7 @@ _DEFAULTS = {
     'CAS_EXTRA_LOGIN_PARAMS': None,
     'CAS_IGNORE_REFERER': False,
     'CAS_LOGOUT_COMPLETELY': True,
-    'CAS_REDIRECT_URL': 'tigerpath.herokuapp.com',
+    'CAS_REDIRECT_URL': '/',
     'CAS_RETRY_LOGIN': False,
     'CAS_SERVER_URL': None,
     'CAS_VERSION': '2',
