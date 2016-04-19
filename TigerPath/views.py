c1d68a04ba02863607f295de3cd013be148e9887
William Hinthorn <hinthorn@princeton.edu>
2016-04-18 22:39:26 -0400

    test3

--- TigerPath/views.py
+++ TigerPath/views.py
@@ -234,6 +234,7 @@ def four_year(request,search):
 	#Check if student is adding a class
 	if request.method == 'POST':
 		added_class = request.POST['listing']
+		added_class = Course.objects.get(listings=added_class)
 		semester = request.POST['semester']
 		semester = time[semester]
 		#add_class(student, added_class, semester)
