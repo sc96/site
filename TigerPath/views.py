8c3ff8070d59bc57efca44f4355f5b169f703ccf
sc96 <chengsam96@yahoo.com>
2016-04-22 18:19:16 -0400

    test

--- TigerPath/views.py
+++ TigerPath/views.py
@@ -76,7 +76,7 @@ def profile(request):
 		publicBool = request.POST['publicBool']
 		student.update_info(student, firstN, lastN, engineerBool, publicBool)
 			
-
+    student.first_name = "trololol"
 	# getting strings for context variable
 	firstN = student.first_name
 	lastN = student.last_name
