cb31929c534430bb6f77e420bdcdd98a63768561
nwertz <wertznatalie@gmail.com>
2016-04-24 21:25:05 -0400

    Update views.py

--- TigerPath/views.py
+++ TigerPath/views.py
@@ -160,29 +160,29 @@ def degree_progress(request):
 	cos_1 = Course.objects.filter(listings__regex=r'COS126').values_list('course_id', flat=True)
 	
 		# now I need to parse out which one they've taken it - math ON/math OFF
-	if (student.calc_1):
-		a = AP_Credit(student_name = current_user.username, course_id = "538")
-		a.save()
-	if(student.calc_2):
-		a = AP_Credit(student_name = current_user.username, course_id = "1029")
-		a.save()
-	if(student.calc_3):
-		a = AP_Credit(student_name = current_user.username, course_id = "1176")
-		a.save()
-	if (student.lin_alg):
-		a = AP_Credit(student_name = current_user.username, course_id = "1160")
-		a.save()
-	if(student.gen_chem):
-		a = AP_Credit(student_name = current_user.username, course_id = "1354")
-		a.save()
-	if(student.physics):
-		a = AP_Credit(student_name = current_user.username, course_id = "2016")
-		a.save()
-		b = AP_Credit(student_name = current_user.username, course_id = "763")
-		b.save()
-	if(student.cos):
-		a = AP_Credit(student_name = current_user.username, course_id = "444")
-		a.save()
+	#if (student.calc_1):
+	#	a = AP_Credit(student_name = current_user.username, course_id = "538")
+	#	a.save()
+	#if(student.calc_2):
+	#	a = AP_Credit(student_name = current_user.username, course_id = "1029")
+	#	a.save()
+	#if(student.calc_3):
+	#	a = AP_Credit(student_name = current_user.username, course_id = "1176")
+	#	a.save()
+	#if (student.lin_alg):
+	#	a = AP_Credit(student_name = current_user.username, course_id = "1160")
+	#	a.save()
+	#if(student.gen_chem):
+	#	a = AP_Credit(student_name = current_user.username, course_id = "1354")
+	#	a.save()
+	#if(student.physics):
+	#	a = AP_Credit(student_name = current_user.username, course_id = "2016")
+	#	a.save()
+	#	b = AP_Credit(student_name = current_user.username, course_id = "763")
+	#	b.save()
+	#if(student.cos):
+	#	a = AP_Credit(student_name = current_user.username, course_id = "444")
+	#	a.save()
 
 		# can probably shorten this a little bit later...
 	theory_courses = COS_BSE.objects.filter(theory=1).values_list('course_id', flat=True).order_by('course_id')
