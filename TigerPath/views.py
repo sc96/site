63c498a000476394ca05f2d051c426893d3a7965
sc96 <chengsam96@yahoo.com>
2016-04-29 00:25:10 -0400

    sc

--- TigerPath/views.py
+++ TigerPath/views.py
@@ -220,16 +220,22 @@ def profile(request):
 	cert1 = student.cert1
 	cert2 = student.cert2
 	cert3 = student.cert3
-	
+
 	calc_1 = student.calc_1
 	calc_2 = student.calc_2
 	calc_3 = student.calc_3
 	lin_alg = student.lin_alg
 
 	ap_dict = {"calc_1": calc_1, "calc_2": calc_2, "calc_3": calc_3, "lin_alg" : lin_alg}
-	context = {'user': current_user.username, 'firstN': firstN, 'lastN': lastN,
+	''' context = {'user': current_user.username, 'firstN': firstN, 'lastN': lastN,
 	 'engineerBool': engineerBool, 'publicBool': publicBool, 'cert1': cert1,
-	 'cert2': cert2, 'cert3': cert3, 'ap_dict': ap_dict}
+	 'cert2': cert2, 'cert3': cert3, 'ap_dict': ap_dict} '''
+
+
+	 context = {'user': current_user.username, 'firstN': firstN, 'lastN': lastN,
+	 'engineerBool': engineerBool, 'publicBool': publicBool, 'cert1': cert1,
+	 'cert2': cert2, 'cert3': cert3, 'calc_1': calc_1,"calc_2": calc_2, "calc_3": calc_3, "lin_alg" : lin_alg } 
+
 	return render(request, 'profile.html', context)
 
 	
