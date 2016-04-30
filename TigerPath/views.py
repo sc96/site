30ba606d7bb83dc0de5aa9d38ab2aa63b1ebd178
tigerpath <princetontigerpath@gmail.com>
2016-04-30 15:25:12 -0400

    Update views.py

--- TigerPath/views.py
+++ TigerPath/views.py
@@ -309,7 +309,7 @@ def degree_progress(request):
 		elif(re.match(r'(ARA|BCS|CHI|CZE|FRE|GER|HEB|HIN|ITA|JPN|KOR|LAT|POL|POR|RUS|SPA|SWA|TUR|TWI|URD)', course.listings)):
 			student_foreign.append(d)
 		
-		if (re.match(r'(MAT3|MAT4|ELE3|ELE4|PHY3|PHY4|ORF3|ORF4)', course.listings)):
+		if (re.match(r'MAT3|MAT4|ELE3|ELE4|PHY3|PHY4|ORF3|ORF4', course.listings)):
 			extra_other.append(d)
 
 	# something to think about: COS 340 can't pop up in "Other" and in "Theory"
