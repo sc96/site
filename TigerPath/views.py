bdcf583c500596273fdf3832b3214af223b23007
sc96 <chengsam96@yahoo.com>
2016-04-30 18:42:43 -0400

    Sc

--- TigerPath/views.py
+++ TigerPath/views.py
@@ -977,18 +977,15 @@ def share(request, shared_user):
 
 
     otherstudent = Student.objects.get(student_id=shared_user)
-    
+
     if (otherstudent.publicBool == 1):
-
-		context = {'user': current_user.username,'shared_user': shared_user, 'fresh_fall': all_frf, 'fresh_spring': all_frs, 
-		'soph_fall': all_sof, 'soph_spring': all_sos, 'junior_fall': all_jrf, 'junior_spring': all_jrs,
-		'senior_fall': all_srf, 'senior_spring': all_srs}#, 'student_outside': student_outside}
-		return render(request, 'share.html', context, )
-
-	else:
-
-		context = {'user': current_user.username}
-		return render(request, 'private.html', context)
+    	context = {'user': current_user.username,'shared_user': shared_user, 'fresh_fall': all_frf, 'fresh_spring': all_frs, 
+    	'soph_fall': all_sof, 'soph_spring': all_sos, 'junior_fall': all_jrf, 'junior_spring': all_jrs,
+    	'senior_fall': all_srf, 'senior_spring': all_srs}#, 'student_outside': student_outside}
+    	return render(request, 'share.html', context, )
+    else:
+    	context = {'user': current_user.username}
+    	return render(request, 'private.html', context)
 
 
 
