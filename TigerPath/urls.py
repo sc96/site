ebe76abed00455f87a54392427776044ecd22b2d
nwertz <wertznatalie@gmail.com>
2016-04-30 13:28:48 -0400

    Update urls.py

--- TigerPath/urls.py
+++ TigerPath/urls.py
@@ -46,6 +46,7 @@ urlpatterns = [
     url(r'^urb/$', views.urb),
     url(r'^qcb/$', views.qcb),
     url(r'^gss/$', views.gss),
+    url(r'^eas/$', views.eas),
     url(r'^outsidecourseapproval/$', views.outside_course_approval),
     url(r'^cosdata/$', views.cos_data),
     url(r'^cosdatasemester/$', views.cos_data_semester),
