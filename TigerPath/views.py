965c135da4309d7cbad35bb8af917e2389e30b3d
William Hinthorn <hinthorn@princeton.edu>
2016-05-02 10:20:29 -0400

    WOOPS

--- TigerPath/views.py
+++ TigerPath/views.py
@@ -172,10 +172,17 @@ def home(request):
 # def notFound(request):
 # 	return render(request, 'pagenotfound.html')
 def handler404(request):
-    response = render_to_response('pagenotfound.html', {},
+    response = render_to_response('404.html', {},
                                   context_instance=RequestContext(request))
     response.status_code = 404
     return response
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
+
 
 
 @login_required # Cas authentication for this url.
