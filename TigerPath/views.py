66750e26bcc58b4a04bc5cf7cffd2de34ade6494
nwertz <wertznatalie@gmail.com>
2016-05-01 20:42:21 -0400

    Update views.py

--- TigerPath/views.py
+++ TigerPath/views.py
@@ -413,6 +413,7 @@ def degree_progress(request):
 		extra_other = title(extra_other)
 		other_on = chain(other_on, save_other)
 		other_on = chain(other_on, extra_other)
+		other_on = chain(other_on, other_other)
 		#for t in all_entries.filter(req="Other").values_list('course_id', flat=True).order_by('course_id'):
 		#	other_on.append(t.course_id)
 		other_off = title(compare_lists(all_courses, other_courses)["differences"])
