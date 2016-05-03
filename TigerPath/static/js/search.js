9e7faabe05d02c32b81a680038e8ae8f1566ba18
William Hinthorn <hinthorn@princeton.edu>
2016-05-02 23:36:19 -0400

    ajax test

--- TigerPath/static/js/search.js
+++ TigerPath/static/js/search.js
@@ -8,8 +8,25 @@ $('#course-search').on('submit', function(event){
 function search_courses() {
     console.log("search_courses!") // sanity check
     console.log($('#course-search-text').val())
-    console.log("Is it the same as?")
-    console.log($('#course-search').val())
+    $.ajax({
+        url : "create_post/", // the endpoint
+        type : "POST", // http method
+        data : { the_query : $('#course-search-text').val() }, // data sent with the post request
+
+        // handle a successful response
+        success : function(json) {
+            $('#course-search-text').val(''); // remove the value from the input
+            console.log(json); // log the returned json to the console
+            console.log("success"); // another sanity check
+        },
+
+        // handle a non-successful response
+        error : function(xhr,errmsg,err) {
+            $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
+                " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        }
+    });
 };
 
 
