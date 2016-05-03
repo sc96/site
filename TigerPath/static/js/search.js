e18fa29812321639318580e616845fa8c2136dcd
William Hinthorn <hinthorn@princeton.edu>
2016-05-02 23:32:26 -0400

    funcetion calling

--- TigerPath/static/js/search.js
+++ TigerPath/static/js/search.js
@@ -2,6 +2,15 @@ $(function() {
 $('#course-search').on('submit', function(event){
 	event.preventDefault();
 	console.log("form submitted ya dumbass")
+	search_courses();
 });
+
+function search_courses() {
+    console.log("search_courses!") // sanity check
+    console.log($('#course-search-text').val())
+    console.log("Is it the same as?")
+    console.log($('#course-search').val())
+};
+
 
 });
